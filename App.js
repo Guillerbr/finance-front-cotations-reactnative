1d0d1b04ab730b81aee2577989f52a7984204632
Guillerbr <ghminvest@gmail.com>
2020-03-14 00:53:25 -0300

    init project

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TextInput
+} from "react-native";
 
 export default function App() {
   return (
+    <KeyboardAvoidingView>
+      <View>
+        <Image source={require('./assets/logo.png')} />
+      </View>
+
+      <View>
+        <TextInput
+          placeholder="Email"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+
+        <TextInput
+          placeholder="Password"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+
+        <TouchableOpacity>
+          <Text>Acess</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text>Create Account</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+    /*
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-        <Text>New application RN</Text>
+      <Text>New application RN</Text>
     </View>
+    */
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
