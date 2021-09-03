3190478b6f4f69742fb0e318760ef3285ca0c7ed
Eduardo Alves <eduardo_alvez51@outlook.com>
2021-09-03 13:49:35 -0300

    start project

--- App.js
+++ App.js
@@ -1,55 +1,21 @@
-import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  KeyboardAvoidingView,
-  TextInput
-} from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <KeyboardAvoidingView>
-      <View>
-        <Image source={require('./assets/logo.png')} />
-      </View>
-
-      <View>
-        <TextInput
-          placeholder="Email"
-          autoCorrect={false}
-          onChangeText={() => {}}
-        />
-
-        <TextInput
-          placeholder="Password"
-          autoCorrect={false}
-          onChangeText={() => {}}
-        />
-
-        <TouchableOpacity>
-          <Text>Acess</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity>
-          <Text>Create Account</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
-    /*
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>New application RN</Text>
+      <StatusBar style="auto" />
     </View>
-    */
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
