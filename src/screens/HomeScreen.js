7a9946f649c3eb8cbd0e5e1fb14871833635c7ac
Eduardo Alves <eduardo_alvez51@outlook.com>
2021-09-04 06:34:20 -0300

    splash config

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -271,7 +271,7 @@ const styles = StyleSheet.create({
     },
     textInput: {
         flex: 1,
-        marginTop: Platform.OS === 'ios' ? 0 : -3,
+        marginTop: Platform.OS === 'ios' ? 0 : -12,
         paddingLeft: 10,
         color: '#05375a',
     },
