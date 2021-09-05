62be42924b039da5d771963d301fcaf3b7886c3a
Eduardo Alves <eduardo_alvez51@outlook.com>
2021-09-05 19:55:10 -0300

    companydetails style

--- src/index.js
+++ src/index.js
@@ -11,12 +11,14 @@ const App = () => {
         ...DefaultTheme.colors,
         colors: {
             background: '#FFF',
+            backgroundCards: '#FFF',
             text: '#333',
             textHeader: '#FFF',
-            backgroundSearch: '#d1d1d1',
             placeholder: '#777',
             primary: Colors.primary,
-            secundary: Colors.secondary
+            secundary: Colors.secondary,
+            backgroundSearch: '#e1e1e1',
+            borderSearch: '#c1c1c1'
         }
     }
 
@@ -24,12 +26,14 @@ const App = () => {
         ...DefaultTheme.colors,
         colors: {
             background: '#333',
-            text: '#fff',
+            backgroundCards: '#444',
+            text: '#d1d1d1',
             textHeader: '#d1d1d1',
             placeholder: '#a1a1a1',
-            backgroundSearch: '#555',
             primary: Colors.primary,
-            secundary: Colors.secondary
+            secundary: Colors.secondary,
+            backgroundSearch: '#555',
+            borderSearch: '#333'
         }
     }
 
